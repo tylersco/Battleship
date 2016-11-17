57a023aed17bc66b67193c34b648d005e5eda764
Chris <chrh5521@colorado.edu>
2016-11-16 19:23:34 -0700

    system inventory skeleton

--- MediaManagementSystem/src/main/java/com/csci4448/MediaManagementSystem/Driver.java
+++ MediaManagementSystem/src/main/java/com/csci4448/MediaManagementSystem/Driver.java
@@ -5,6 +5,7 @@ import  com.csci4448.MediaManagementSystem.controller.*;
 public class Driver {
 
     public static void main(String[] args) {
+
         MainController c = new MainController();
     }
 
--- MediaManagementSystem/src/main/java/com/csci4448/MediaManagementSystem/model/media/SystemInventory.java
+++ MediaManagementSystem/src/main/java/com/csci4448/MediaManagementSystem/model/media/SystemInventory.java
@@ -1,8 +1,15 @@
 package com.csci4448.MediaManagementSystem.model.media;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SystemInventory {
 
-    private static SystemInventory systemInventory;
+    private static SystemInventory systemInventory = null;
+
+    private ArrayList<Media> mediaList;
+
+    private HashMap<String, ArrayList<String>> typeGenreMap;
 
     private SystemInventory() {}
 
@@ -10,6 +17,26 @@ public class SystemInventory {
         if (systemInventory == null)
             systemInventory = new SystemInventory();
         return systemInventory;
+    }
+
+    public ArrayList<Media> searchInventory(String searchText){
+
+    }
+
+    public void addMedia(Media media){
+
+    }
+
+    public void deleteMedia(Media media){
+
+    }
+
+    public void addGenre(String genre){
+
+    }
+
+    public void addType(String type, String genre){
+
     }
 
 }
