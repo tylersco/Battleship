4d504b80aa79b8f3e81b8e9c06c9a54673751a11
Sean Moss <semo0788@colorado.edu>
2016-11-27 17:17:51 -0700

    Minor changes, mostly a transfer commit

--- MediaManagementSystem/src/main/java/com/csci4448/MediaManagementSystem/ui/design/Confirmation.java
+++ MediaManagementSystem/src/main/java/com/csci4448/MediaManagementSystem/ui/design/Confirmation.java
@@ -6,7 +6,11 @@ public enum Confirmation {
     RENTMEDIA ("Are you sure you want to rent this?", "Yes", "No"),
     SELLMEDIA ("Are you sure you want to sell this?", "Yes", "No"),
     RETURNMEDIA ("Are you sure you want to return this?", "Yes", "No"),
-    LOGOUT ("Are you sure you want to sign out?", "Sign out", "Cancel"),;
+    LOGOUT ("Are you sure you want to sign out?", "Sign out", "Cancel"),
+    ADMINSAVE ("Are you sure you want to save changes to the media?", "Yes", "No"),
+    ADMINNEW ("Are you sure you want to create new media?", "Yes", "No"),
+    ADMINCANCEL ("Are you sure you want to discard changes to the media?", "Yes", "No"),
+    ;
 
     private String text;
     private String confirmText;
